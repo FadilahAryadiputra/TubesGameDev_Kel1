ebef390e4fb84a3fb2a9348839646fa63394c357
Ariach_Dh1yl <fadilaharyadiputra01@gmail.com>
2022-02-14 16:28:42 +0700

    Add PlayerJump, Parallax, Distance
    - Add jump control for player when grounded
    - Add parallax for looping ground and background
    - Add distance for score

--- 2D Take a Run/Assets/Scripts/PlayerController.cs
+++ 2D Take a Run/Assets/Scripts/PlayerController.cs
@@ -10,16 +10,46 @@ public class PlayerController : MonoBehaviour
     public float startDashTime;
     private int direction;
 
+    public bool isDead = false;
+
+    public bool isGrounded;//is my player colliding with the floor?
+    public bool isJumping;//is my player jumping or can he jump?
+    public float jumpForceValue = 200;
+    public float jumpForce;//how much force is the player using to jump. gravity is always pushing down on player so he needs X amount of force to jump
+    public float sidSpd;//how fast is my player moving left or right (Left this will be negative, RIght this willbe positive)
+	public float moveSpd;//A variable were going to ue later on in the tutorial to increase players speed or decrease it
+
+    public Vector2 velocity;
+    public float distance = 0;
+    public float maxXVelocity = 100;
+    public float maxAcceleration = 10;
+    public float acceleration = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         dashTime = startDashTime;
+        isJumping=false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 pos = transform.position;
+
+        sidSpd = Input.GetAxis ("Horizontal") * 5 ;//Input.GetAxis will grab the Axis "Horizontal" in this case.
+		// to create axis  hit the Edit tab > Project Setting > Input
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))//Input.GetKeyDown() waits for the user to press a key once
+		{
+			if(isGrounded == true)//if my player is grounded do whats in the barckets
+			{
+				isJumping=true;
+				jumpForce = jumpForceValue;//jump force equals 200 because our Player has attempted to jump
+			}
+		}
+
         if(direction == 0 ){
             if(Input.GetKeyDown(KeyCode.LeftArrow)){
                 direction = 1;
@@ -52,4 +82,48 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
+    void FixedUpdate()
+	{
+        Vector2 pos = transform.position;
+
+		rb.AddForce (new Vector3(sidSpd,jumpForce,0));//Add a force to my players RigidBody.
+		//The force must be a vector 3 for this script so it adds force in 3 Dimensions.
+
+        distance += velocity.x * Time.fixedDeltaTime;
+
+        if (isGrounded)
+        {
+            float velocityRatio = velocity.x / maxXVelocity;
+            acceleration = maxAcceleration * (1 - velocityRatio);
+
+            velocity.x += acceleration * Time.fixedDeltaTime;
+            if (velocity.x >= maxXVelocity)
+            {
+                velocity.x = maxXVelocity;
+            }
+        }
+
+
+        transform.position = pos;
+	}
+    void OnCollisionEnter2D(Collision2D col)
+	{
+		Debug.Log ("you hit something: " + col.collider.tag);//basic print message for debugging purposes
+		if (col.collider.tag == "Ground")//if the object you collided withs tag is ground your player is on the floor
+		{
+			isGrounded = true;///so grounded must be true because Player has hit the floor.
+            isJumping = false;
+		}
+	}
+    void OnCollisionExit2D(Collision2D col)
+	{
+		Debug.Log ("you left something: " + col.collider.tag);//basic print message for debugging purposes
+		if (col.collider.tag == "Ground")//if the object you WERE colliding withs tag is ground your player is leaving the floor
+		{
+			isGrounded = false;//Player is no longer grounded
+			jumpForce = 0;//the Player no longer has any force in his jump when he is in the air
+		}
+
+	}
 }
